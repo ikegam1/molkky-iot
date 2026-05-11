5c1c7751f4b19e4e1ca46df6ba5cc9de7cf31fb3
ko1 <ko1@ko1noMac-mini.local>
2026-05-11 22:27:41 +0900

    Match working sample strictly and lower SPI frequency

--- epaper_driver.py
+++ epaper_driver.py
@@ -69,7 +69,7 @@ class EPD_2in7_V2_Landscape:
         self.LUT_DATA_4Gray = LUT_DATA_4GRAY
 
         self.spi = SPI(1)
-        self.spi.init(baudrate=2_000_000)
+        self.spi.init(baudrate=1_000_000)
 
         self.buffer = bytearray(self.native_height * self.native_width // 8)
         self.fb = framebuf.FrameBuffer(self.buffer, self.width, self.height, framebuf.MONO_VLSB)
@@ -112,7 +112,7 @@ class EPD_2in7_V2_Landscape:
         self.reset_pin.value(1)
         self.delay_ms(200)
         self.reset_pin.value(0)
-        self.delay_ms(2)
+        self.delay_ms(10) # increased from 2
         self.reset_pin.value(1)
         self.delay_ms(200)
 
@@ -177,20 +177,13 @@ class EPD_2in7_V2_Landscape:
         self.send_command(0x12)  # SWRESET
         self.ReadBusy()
 
-        # Added explicit RAM address boundaries (from sample's 4Gray/V2 logic)
-        self.send_command(0x44)  # RAM-X start/end
-        self.send_data(0x00)
-        self.send_data(0x15)     # 22 bytes
-
-        self.send_command(0x45)  # RAM-Y start/end
+        self.send_command(0x45)  # RAM-Y start/end: 0..263
         self.send_data(0x00)
         self.send_data(0x00)
         self.send_data(0x07)
-        self.send_data(0x01)     # 264 lines
+        self.send_data(0x01)
 
-        self.send_command(0x4E)  # RAM-X counter
-        self.send_data(0x00)
-        self.send_command(0x4F)  # RAM-Y counter
+        self.send_command(0x4F)  # RAM-Y counter = 0
         self.send_data(0x00)
         self.send_data(0x00)
 
@@ -238,10 +231,7 @@ class EPD_2in7_V2_Landscape:
     def clear(self):
         width_bytes = self.native_width // 8
         height = self.native_height
-        # Initialize both RAM areas to White
         self.send_command(0x24)
-        self.send_data_buffer([0xFF] * width_bytes * height)
-        self.send_command(0x26)
         self.send_data_buffer([0xFF] * width_bytes * height)
         self.TurnOnDisplay()
 
